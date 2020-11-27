661ad4ff36d1d89aa082bbd3f8bda6440750472e
iamyakose <iamyakose@outlook.com>
2020-11-27 14:09:12 +0100

    First Commit in local repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VC_Git.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
